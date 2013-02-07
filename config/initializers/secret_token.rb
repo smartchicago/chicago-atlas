6ca8033f9e39cbee5709cf3825a7bff958bbd395
Derek Eder <derek.eder@gmail.com>
2013-02-07 10:56:39 -0600

    initial commit - Rails3 with postgresql and mongo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChicagoAtlas::Application.config.secret_token = 'c619e27c314c47e6079b5eff7516b1520f3c4201377ea7216b683d6c1db824fc064d7dc4d030f390f554b17ac69623b22cb74510ae2f7518c00a128efdea1960'
