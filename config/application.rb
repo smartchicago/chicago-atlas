d5a49417fc145a57dc3ea492b6d5c1d94a031ebe
Derek Eder <derek.eder@gmail.com>
2013-02-08 10:13:50 -0600

    disabling init precompile for heroku

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,9 @@ module ChicagoAtlas
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # If you are deploying Rails 3.1 on Heroku force your application to not access the DB
+    # or load models when precompiling your assets.
+    config.assets.initialize_on_precompile = false
   end
 end
