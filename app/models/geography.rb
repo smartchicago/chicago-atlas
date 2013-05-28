833562d41650c012661e081a0b991b72f54451cc
Derek Eder <derek.eder@gmail.com>
2013-05-27 21:40:01 -0400

    showing population as column bar charts, showing total population for each area

--- app/models/geography.rb
+++ app/models/geography.rb
@@ -7,4 +7,17 @@ class Geography < ActiveRecord::Base
     my_centroid = JSON.parse(centroid)
     [my_centroid[1],my_centroid[0]]
   end
+
+  def pop_total(year)
+    pop = Statistic.joins('INNER JOIN datasets ON datasets.id = statistics.dataset_id')
+                     .where("datasets.slug = 'population_all_total'") # brittle way to get this data
+                     .where("geography_id = #{id}")
+                     .where("year = #{year}").first
+
+    unless pop.nil?
+      pop.value.to_i
+    else
+      0
+    end
+  end
 end
