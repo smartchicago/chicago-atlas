9a6770df7e736f730977d4f70026c041ea7347ec
Derek Eder <derek.eder@gmail.com>
2013-06-17 17:41:46 -0500

    removed rounding for chart values, coloring death labels when value >= chicago avg

--- app/assets/javascripts/chart_helper.js
+++ app/assets/javascripts/chart_helper.js
@@ -78,7 +78,7 @@ ChartHelper.create = function(element, type, seriesData, startDate, yearRange, p
             if (point.point.low != null && point.point.high != null && point.point.low != 0 && point.point.high != 0)
               s += "<br /><span style=\"color: " + point.series.color + "\">" + point.series.name + ":</span> " + point.point.low + percentSuffix + " - " + point.point.high + percentSuffix;
             else
-              s += "<br /><span style=\"color: " + point.series.color + "\">" + point.series.name + ":</span> " + Highcharts.numberFormat(point.y, 0) + percentSuffix;
+              s += "<br /><span style=\"color: " + point.series.color + "\">" + point.series.name + ":</span> " + point.y + percentSuffix;
           });
           return s;
         },
