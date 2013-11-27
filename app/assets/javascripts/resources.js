6898fd9ee34eab159dd60ea02a94fa4a9a1f6080
Derek Eder <derek.eder@gmail.com>
2013-11-27 17:32:52 -0600

    enabled font-awesome for icons, implemented map/list toggle on resources page

--- app/assets/javascripts/resources.js
+++ app/assets/javascripts/resources.js
@@ -37,18 +37,23 @@ var ResourcesLib = {
         return false;
       });
 
-      // by default, show all
-      ResourcesLib.render_category('all');
+      if ($.address.parameter('category') != null)
+        ResourcesLib.render_category($.address.parameter('category'));
+      else
+        ResourcesLib.render_category('all'); // by default, show all
+
+      // toggle map / list view
+      ResourcesLib.toggle_view($.address.parameter('view_mode'));
       
     });
   },
 
   render_category: function(category){
-    
     // clean up and highlight selected
     ResourcesLib.reset_view();
     $("#categories li").attr("class", "");
     $("[data-category='" + category + "']").parent().attr("class", "active");
+    $.address.parameter('category', category);
 
     var resources = [];
     for (var i=0;i<ResourcesLib.resources_list.length;i++)
@@ -82,6 +87,23 @@ var ResourcesLib = {
         )).bindPopup(Mustache.render(map_template, resources[i]))
       );
     }
+  },
+
+  toggle_view: function(view_mode){
+    console.log(view_mode);
+    if (view_mode == 'list') {
+      $("#toggle_view").html("Map <i class='icon-map-marker'></i>");
+      $.address.parameter('view_mode', 'list');
+      $("#map_resources").hide();
+      $("#table_resources").show();
+    }
+    else {
+      $("#toggle_view").html("List <i class='icon-list'></i>");
+      $.address.parameter('view_mode', 'map');
+      $("#map_resources").show();
+      $("#table_resources").hide();
+    }
+
   },
 
   reset_view: function() {
