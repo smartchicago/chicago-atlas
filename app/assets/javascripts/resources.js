b3166fd7eff49263ea2a765e481dd25c43fe6c43
Derek Eder <derek.eder@gmail.com>
2013-11-27 16:57:17 -0600

    outputing resources_json by category grouping, implemented category browser

--- app/assets/javascripts/resources.js
+++ app/assets/javascripts/resources.js
@@ -1,6 +1,8 @@
 var ResourcesLib = ResourcesLib || {};
 var ResourcesLib = {
 
+  resources_list: [],
+
   init: function(geography_geometry, geography_centroid, url_fragment) {
     var geojsonFeature = {
         "type": "Feature",
@@ -15,52 +17,76 @@ var ResourcesLib = {
     bounds[2] = (bounds[2] + "").replace(".",",");
     bounds[3] = (bounds[3] + "").replace(".",",");
 
-    var category_list = [];
-
     $.getJSON("/resources" + url_fragment + "/" + bounds.join("/") + ".json", function(resources){
       
-      for(var i=0;i<resources.length;i++){
-        var categories = eval(resources[i]['categories'])
-        for (var j=0;j<categories.length;j++){
-          if (category_list.indexOf(categories[j]) == -1) {
-            category_list.push(categories[j]);
-          }
-        }
-
-        // clean up source data
-        if (resources[i]['phone'].indexOf("N/A") >= 0 ) resources[i]['phone'] = '';
-
-        // populate table and map
-        var table_template = "\
-          <tr>\
-            <td><strong>{{organization_name}}</strong><br />{{program_name}}</td>\
-            <td>{{address}}</td>\
-            <td>{{phone}}</td>\
-          </tr>\
-          ";
-        $("#intervention_list").append(Mustache.render(table_template, resources[i]));
-
-        var map_template = "\
-          <b>{{organization_name}}</b>\
-          <br />{{program_name}}<br/>\
-          {{address}}<br/>\
-          {{phone}}\
-          ";
-
-        LeafletLib.addMarker(
-          (new L.Marker(
-            new L.LatLng( resources[i]['latitude'], resources[i]['longitude'] )
-          )).bindPopup(Mustache.render(map_template, resources[i]))
-        );
-      }
+      ResourcesLib.resources_list = resources;
 
       // populate list of categories
-      category_list = category_list.sort();
-      for (var j=0;j<category_list.length;j++) {
-        $("#categories").append("<li><a href='#'>" + ResourcesLib.humanize(category_list[j]) + "</a></li>");
+      for (var i=0;i<ResourcesLib.resources_list.length;i++) {
+        $("#categories").append("\
+          <li><a href='#' data-category='" + ResourcesLib.resources_list[i]['category'] + "'>\
+          " + ResourcesLib.humanize(ResourcesLib.resources_list[i]['category']) + "\
+          <span class='badge pull-right'>" + ResourcesLib.resources_list[i]['resources'].length + "</span>\
+          </a>\
+          </li>");
       }
+
+      // bind clickable event for categories
+      $("#categories a").click(function(){
+        ResourcesLib.render_category($(this).attr('data-category'));
+        return false;
+      });
+
+      // by default, show all
+      ResourcesLib.render_category('all');
+      
     });
-    LeafletLib.filterMarkers({ geojson: geojsonFeature });
+  },
+
+  render_category: function(category){
+    
+    // clean up and highlight selected
+    ResourcesLib.reset_view();
+    $("#categories li").attr("class", "");
+    $("[data-category='" + category + "']").parent().attr("class", "active");
+
+    var resources = [];
+    for (var i=0;i<ResourcesLib.resources_list.length;i++)
+      if (ResourcesLib.resources_list[i]['category'] == category)
+        resources = ResourcesLib.resources_list[i]['resources'];
+
+    for (var i=0;i<resources.length;i++) {
+      // clean up source data
+      if (resources[i]['phone'].indexOf("N/A") >= 0 ) resources[i]['phone'] = '';
+
+      // populate table and map
+      var table_template = "\
+        <tr>\
+          <td><strong>{{organization_name}}</strong><br />{{program_name}}</td>\
+          <td>{{address}}</td>\
+          <td>{{phone}}</td>\
+        </tr>\
+        ";
+      $("#intervention_list").append(Mustache.render(table_template, resources[i]));
+
+      var map_template = "\
+        <b>{{organization_name}}</b>\
+        <br />{{program_name}}<br/>\
+        {{address}}<br/>\
+        {{phone}}\
+        ";
+
+      LeafletLib.addMarker(
+        (new L.Marker(
+          new L.LatLng( resources[i]['latitude'], resources[i]['longitude'] )
+        )).bindPopup(Mustache.render(map_template, resources[i]))
+      );
+    }
+  },
+
+  reset_view: function() {
+    LeafletLib.clearMarkers(); 
+    $("#intervention_list").html("");
   },
 
   humanize: function(property) {
