527246b50a7945e6c714219415a54169d831033c
Cathy Deng <cathyd125@gmail.com>
2014-11-04 17:23:11 -0600

    add provider accessible attributes

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,4 +1,4 @@
 class Provider < ActiveRecord::Base
-  attr_accessible :src_id, :name, :slug, :primary_type, :sub_type, :addr_street, :addr_city, :addr_zip, :ownership_type, :contact_email, :contact_phone, :lat_long
+  attr_accessible :src_id, :name, :slug, :primary_type, :sub_type, :addr_street, :addr_city, :addr_zip, :ownership_type, :contact_email, :contact_phone, :lat_long, :description, :phone, :url, :report_url, :report_name
 
 end
