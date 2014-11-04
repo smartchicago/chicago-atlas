b1fa2ccd5cc68e4bde06cc978d89eb4d74056778
Cathy Deng <cathyd125@gmail.com>
2014-11-04 17:23:53 -0600

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140811212217) do
+ActiveRecord::Schema.define(:version => 20141104231636) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -107,6 +107,11 @@ ActiveRecord::Schema.define(:version => 20140811212217) do
     t.datetime "created_at",     :null => false
     t.datetime "updated_at",     :null => false
     t.string   "lat_long"
+    t.text     "description"
+    t.string   "phone"
+    t.string   "url"
+    t.string   "report_url"
+    t.string   "report_name"
   end
 
   create_table "statistics", :force => true do |t|
