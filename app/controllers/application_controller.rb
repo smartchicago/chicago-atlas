8935b175c0a33156124988025094127bd2dc9268
rubyonrocks <niels@onrocks.co>
2016-12-16 22:00:41 +0100

    Adds Home Layout to the Dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  layout :layout_by_resource
+
+  protected
+
+  def layout_by_resource
+    if devise_controller? && action_name == "new"
+      "application"
+    else
+      "home"
+    end
+  end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   before_action :authenticate_user!
 
+
   def index
   end
 end
