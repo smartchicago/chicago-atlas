e15604a5305ae64a5943a0540ef1c8dd2c35205a
TigerDevCom <tommy327bowe@gmail.com>
2017-01-19 11:12:21 -0800

    resources model spec fix

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,16 +3,30 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  describe "factory validation check" do
+    context "valid factory" do
+      it "should be valid factory" do
+        expect(FactoryGirl.create(:user)).to be_valid
+      end
+    end
 
-  it "has valid factory" do
-    user = FactoryGirl.create(:user)
-    expect(user).to be_valid
+    context "invalid factory" do
+      it "is invalid without name" do
+        expect(FactoryGirl.build(:user, name: nil)).not_to be_valid
+      end
+
+      it "is invalid without email" do
+        expect(FactoryGirl.build(:user, email: nil)).not_to be_valid
+      end
+    end
   end
 
-  describe "Active record associations" do
+  describe "ActiveRecord associations" do
     it { should have_many(:uploaders).dependent(:destroy) }
+  end
+
+  describe "Validations" do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:email) }
   end
-
 end
