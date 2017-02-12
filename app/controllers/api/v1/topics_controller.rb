60d66f4f2a24f81588107c76b4a4166a498bd471
TigerDevCom <tommy327bowe@gmail.com>
2017-02-12 15:47:29 -0800

    fix

--- app/controllers/api/v1/topics_controller.rb
+++ app/controllers/api/v1/topics_controller.rb
@@ -27,6 +27,22 @@ module Api
         render json: @data
       end
 
+      api :GET, '/topic_area/:year/:indicator_id', 'Fetch detailed data of topic'
+      param :year, String, :desc => 'year', :required => true
+      param :indicator_id, String, :desc => 'indicator id', :required => true
+      formats ['json']
+      description <<-EOS
+        == Fetch detailed data for indicatior and year in city area
+        response data has detailed data for indicator and year
+      EOS
+      def area_show
+        year = params[:year]
+        slug = params[:indicator_id]
+        city = GeoGroup.find_by_geography('City')
+        @data = Resource.where("year_from <= ? AND year_to >= ?", year, year).where(indicator_id: slug).where.not(geo_group_id: city.id)
+        render json: @data
+      end
+
       api :GET, '/topic_detail/:indicator_id', 'Fetch detailed data of topic for trend'
       param :indicator_id, String, :desc => 'indicator id', :required => true
       formats ['json']
