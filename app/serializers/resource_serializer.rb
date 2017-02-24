5ca6065424fbff0f813e0157912ce0641ea3fe9a
TigerDevCom <tommy327bowe@gmail.com>
2017-02-23 19:11:56 -0800

    feature/fix topic

--- app/serializers/resource_serializer.rb
+++ app/serializers/resource_serializer.rb
@@ -1,8 +1,9 @@
 class ResourceSerializer < ActiveModel::Serializer
-  attributes :id, :uploader_id, :category_group_id, :indicator_id, :year_from, :year_to, :geo_group, :demo_group, :number, :cum_number, :ave_annual_number, :crude_rate, :lower_95ci_crude_rate, :upper_95ci_crude_rate, :percent, :lower_95ci_percent, :upper_95ci_percent, :weight_number, :weight_percent, :lower_95ci_weight_percent, :upper_95ci_weight_percent,:map_key, :flag
+  attributes :id, :uploader, :category_group, :sub_category, :indicator, :year_from, :year_to, :geo_group, :demo_group, :number, :cum_number, :ave_annual_number, :crude_rate, :lower_95ci_crude_rate, :upper_95ci_crude_rate, :percent, :lower_95ci_percent, :upper_95ci_percent, :weight_number, :weight_percent, :lower_95ci_weight_percent, :upper_95ci_weight_percent,:map_key, :flag
 
   belongs_to :uploader
   belongs_to :category_group
+  belongs_to :sub_category
   belongs_to :demo_group
   belongs_to :geo_group
   belongs_to :indicator
