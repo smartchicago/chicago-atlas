bcfd6188ec944038fc67baae73771e399923c3f2
TigerDevCom <tommy327bowe@gmail.com>
2017-03-09 04:22:13 -0800

    id to slug

--- app/controllers/api/v1/topics_controller.rb
+++ app/controllers/api/v1/topics_controller.rb
@@ -28,7 +28,7 @@ module Api
         render json: @data, each_serializer: TopicCitySerializer
       end
 
-      api :GET, '/topic_area/:year/:indicator_id', 'Fetch detailed data of topic'
+      api :GET, '/topic_area/:year/:indicator_slug', 'Fetch detailed data of topic'
       param :year, String, :desc => 'year', :required => true
       param :indicator_id, String, :desc => 'indicator id', :required => true
       formats ['json']
@@ -37,23 +37,25 @@ module Api
         response data has detailed data for indicator and year
       EOS
       def area_show
-        year  = params[:year]
-        slug  = params[:indicator_id]
-        city  = GeoGroup.find_by_geography('City')
-        @data = Resource.where("year_from <= ? AND year_to >= ?", year, year).where(indicator_id: slug).where.not(geo_group_id: city.id)
+        year          = params[:year]
+        slug          = params[:indicator_slug]
+        city          = GeoGroup.find_by_geography('City')
+        indicator_id  = Indicator.find_by_slug(slug).id
+        @data         = Resource.where("year_from <= ? AND year_to >= ?", year, year).where(indicator_id: slug).where.not(geo_group_id: city.id)
         render json: @data, each_serializer: TopicAreaSerializer
       end
 
-      api :GET, '/topic_detail/:indicator_id', 'Fetch detailed data of topic for trend'
-      param :indicator_id, String, :desc => 'indicator id', :required => true
+      api :GET, '/topic_detail/:indicator_slug', 'Fetch detailed data of topic for trend'
+      param :indicator_id, String, :desc => 'indicator slug', :required => true
       formats ['json']
       description <<-EOS
         == Fetch detailed data for indicatior
         response data has detailed data for indicator(for trend all year data)
       EOS
       def trend
-        slug  = params[:indicator_id]
-        @data = Resource.where(indicator_id: slug)
+        slug          = params[:indicator_slug]
+        indicator_id  = Indicator.find_by_slug(slug).id
+        @data         = Resource.where(indicator_id: indicator_id)
         render json: @data, each_serializer: TopicDetailSerializer
       end
 
@@ -87,19 +89,20 @@ module Api
         render json: @data
       end
 
-      api :GET, '/topic_info/:geo_slug/:indicator_id', 'Fetch detailed data of topic for community area'
-      param :indicator_id, String, :desc => 'indicator id', :required => true
+      api :GET, '/topic_info/:geo_slug/:indicator_slug', 'Fetch detailed data of topic for community area'
+      param :indicator_slug, String, :desc => 'indicator slug', :required => true
       formats ['json']
       description <<-EOS
         == Fetch detailed data for community area
       EOS
       def info
-        indicator_id  = params[:indicator_id]
-        geo_slug      = params[:geo_slug]
-        geo_group_id  = GeoGroup.find_by_slug(geo_slug)
-        chicago_id    = GeoGroup.find_by_slug('chicago')
-        @area_data    = Resource.where(indicator_id: indicator_id, geo_group_id: geo_group_id)
-        @city_data    = Resource.where(indicator_id: indicator_id, geo_group_id: chicago_id)
+        indicator_slug = params[:indicator_slug]
+        geo_slug       = params[:geo_slug]
+        geo_group_id   = GeoGroup.find_by_slug(geo_slug)
+        indicator_id   = Indicator.find_by_slug(indicator_slug)
+        chicago_id     = GeoGroup.find_by_slug('chicago')
+        @area_data     = Resource.where(indicator_id: indicator_id, geo_group_id: geo_group_id)
+        @city_data     = Resource.where(indicator_id: indicator_id, geo_group_id: chicago_id)
         render :json => {
           :area_data => @area_data,
           :city_data => @city_data
