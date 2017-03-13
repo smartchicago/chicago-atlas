3c9a5fcd272ca967ef4755ca98759fe88d9a1451
TigerDevCom <tommy327bowe@gmail.com>
2017-03-13 09:06:55 -0700

    demo list fix

--- app/controllers/api/v1/topics_controller.rb
+++ app/controllers/api/v1/topics_controller.rb
@@ -75,7 +75,7 @@ module Api
         demo_slug       = params[:demo_slug]
         indicator_slug  = params[:indicator_slug] 
         # @data           = Resource.select { |d| (d.demo_group.slug == demo_slug unless d.demo_group.blank?) && (d.indicator.slug == indicator_slug) }
-        @data           = Resource.select { |d| (d.demo_group.demography == demo_slug unless d.demo_group.blank?) && (d.indicator.slug == indicator_slug) }
+        @data           = Resource.select { |d| (d.demo_group.demography.downcase == demo_slug unless d.demo_group.blank?) && (d.indicator.slug == indicator_slug) }
         render json: @data, each_serializer: TopicDemoSerializer 
       end
 
