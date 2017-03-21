aa10403d717df068feb710b8700c597112f7f51d
German Hernandez <muygrafico@gmail.com>
2017-03-20 20:41:46 -0500

    alejo fix

--- app/controllers/api/v1/topics_controller.rb
+++ app/controllers/api/v1/topics_controller.rb
@@ -84,10 +84,12 @@ module Api
       # end
 
       def demo
-         demo_slug       = params[:demo_slug]
-         indicator_slug  = params[:indicator_slug]
-         data            = Resource.select { |d| (d.demo_group.demography.downcase == demo_slug.downcase unless d.demo_group.blank?) && (d.indicator.slug == indicator_slug) }
-         render json: data, each_serializer: TopicDemoSerializer
+        demo_slug       = params[:demo_slug]
+        indicator_slug  = params[:indicator_slug]
+        #  data            = Resource.select { |d| (d.demo_group.demography.downcase == demo_slug.downcase unless d.demo_group.blank?) && (d.indicator.slug == indicator_slug) }
+
+        data = Resource.joins(:demo_group, :indicator).where('lower(demo_groups.demography) = ? AND indicators.slug = ?', demo_slug.downcase, indicator_slug)
+        render json: data, each_serializer: TopicDemoSerializer
       end
 
       api :GET, '/topic_recent/:indicator_slug', 'Fetch detailed data of topic'
