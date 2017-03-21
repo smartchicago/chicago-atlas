689ee5a8b284ae4c70e213a3a5983758bfbcab43
niliss <niels@onrocks.co>
2017-03-21 01:40:49 +0100

    adds rack compression middleware to the application

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ Bundler.require(*Rails.groups)
 
 module Parrolabs
   class Application < Rails::Application
+    config.middleware.use Rack::Deflater
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
