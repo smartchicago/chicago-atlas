01c221680275a272f8d7a1c313fc9cb0b02a316b
TigerDevCom <tommy327bowe@gmail.com>
2017-03-22 23:23:36 -0700

    fix for topic_recent

--- app/serializers/topic_recent_serializer.rb
+++ app/serializers/topic_recent_serializer.rb
@@ -1,10 +1,7 @@
 class TopicRecentSerializer < ActiveModel::Serializer
   cache key: 'TopicRecent', expires_in: 3.hours
   attributes :id, :category_group_name, :sub_category_name,
-    :year_from, :year_to, :demo_group, :number, :cum_number, :ave_annual_number,
-    :crude_rate, :lower_95ci_crude_rate, :upper_95ci_crude_rate, :percent,
-    :lower_95ci_percent, :upper_95ci_percent, :weight_number,
-    :weight_percent, :lower_95ci_weight_percent, :upper_95ci_weight_percent
+    :year_from, :year_to, :demo_group,:percent, :weight_number
 
   def category_group_name
     object.category_group.name
