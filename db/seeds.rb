daef19f9ae669254b0f52575e55ddfefaab521fe
TigerDevCom <tommy327bowe@gmail.com>
2017-05-20 09:09:39 -0700

    Update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,15 +5,3 @@ user.email    = 'admin@chicagohealthatlas.com'
 user.password = '1234567890'
 user.name     = 'admin'
 user.save!
-
-
-
-#  Upload Geography
-chicago = Geography.new(
-  :geo_type => "City",
-  :name => 'Chicago',
-  :slug => 'chicago',
-  :geometry => ''
-)
-chicago.id = 100
-chicago.save!
