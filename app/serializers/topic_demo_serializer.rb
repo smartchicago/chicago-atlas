d06d29d99ae8acf61e4f0bb9e3162fc55ce1373e
Marco Perez <marco.perez@domandtom.com>
2017-06-29 17:19:32 -0300

    fixing topic demo adding age_adj_rate to be used by preventable-hospitalizations

--- app/serializers/topic_demo_serializer.rb
+++ app/serializers/topic_demo_serializer.rb
@@ -1,6 +1,15 @@
 class TopicDemoSerializer < ActiveModel::Serializer
   cache key: 'TopicDemo', expires_in: 3.hour
-  attributes :id, :category_group_name, :sub_category_name, :indicator, :year_from, :year_to, :demo_group, :demography, :number, :cum_number, :ave_annual_number, :crude_rate, :lower_95ci_crude_rate, :upper_95ci_crude_rate, :percent, :lower_95ci_percent, :upper_95ci_percent, :weight_number, :weight_percent, :lower_95ci_weight_percent, :upper_95ci_weight_percent
+  attributes :id, :category_group_name, 
+    :sub_category_name, :indicator, 
+    :year_from, :year_to, :demo_group, 
+    :demography, :number, :cum_number, 
+    :ave_annual_number, :crude_rate, 
+    :lower_95ci_crude_rate, :upper_95ci_crude_rate, 
+    :percent, :lower_95ci_percent, :upper_95ci_percent, 
+    :weight_number, :weight_percent, 
+    :lower_95ci_weight_percent, :upper_95ci_weight_percent,
+    :age_adj_rate
 
   def demo_group
     object.demo_group.name unless object.demo_group.blank?
