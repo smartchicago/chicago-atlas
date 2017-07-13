b36c3e6ceb5e6db2a16d9280ca591f3f138d3041
Marco Perez <marco.perez@domandtom.com>
2017-07-13 12:54:04 -0300

    refactor exlucde sub category indices

--- app/models/sub_category.rb
+++ app/models/sub_category.rb
@@ -1,5 +1,9 @@
 class SubCategory < ActiveRecord::Base
   belongs_to :category_group
   has_many :indicators
-  scope :with_indicators, -> { joins(:indicators).where.not('indicators.slug': ['child-opportunity-index','economic-hardship']).distinct }
+
+  SLUG_INDICES = 'indices'
+
+  scope :exclude_indices, -> { where.not('sub_categories.slug': SLUG_INDICES) }
+  scope :with_indicators, -> { exclude_indices.joins(:indicators).distinct }
 end
--- app/serializers/community_area_detail_serializer.rb
+++ app/serializers/community_area_detail_serializer.rb
@@ -2,8 +2,8 @@ class CommunityAreaDetailSerializer < ActiveModel::Serializer
   attributes :id, :name, :slug, :sub_categories
 
   def sub_categories
-    object.sub_categories.map do |sub_category|
-      CommunityAreaSubCategorySerializer.new(sub_category, scope: scope, root: false, geo_id: @instance_options[:geo_id]) 
+    object.sub_categories.exclude_indices.map do |sub_category|
+      CommunityAreaSubCategorySerializer.new(sub_category, scope: scope, root: false, geo_id: @instance_options[:geo_id])
     end
   end
 end
