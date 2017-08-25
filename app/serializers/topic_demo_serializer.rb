b7017633a9f8f92b2890eb24f111c481e01e69b6
Marco Perez <marco.perez@domandtom.com>
2017-08-25 16:55:06 -0300

    add year to demo serializer

--- app/serializers/topic_demo_serializer.rb
+++ app/serializers/topic_demo_serializer.rb
@@ -1,13 +1,13 @@
 class TopicDemoSerializer < ActiveModel::Serializer
   cache key: 'TopicDemo', expires_in: 3.hour
-  attributes :id, :category_group_name, 
-    :sub_category_name, :indicator, 
-    :year_from, :year_to, :demo_group, 
-    :demography, :number, :cum_number, 
-    :ave_annual_number, :crude_rate, 
-    :lower_95ci_crude_rate, :upper_95ci_crude_rate, 
-    :percent, :lower_95ci_percent, :upper_95ci_percent, 
-    :weight_number, :weight_percent, 
+  attributes :id, :category_group_name,
+    :sub_category_name, :indicator,
+    :year_from, :year_to, :year, :demo_group,
+    :demography, :number, :cum_number,
+    :ave_annual_number, :crude_rate,
+    :lower_95ci_crude_rate, :upper_95ci_crude_rate,
+    :percent, :lower_95ci_percent, :upper_95ci_percent,
+    :weight_number, :weight_percent,
     :lower_95ci_weight_percent, :upper_95ci_weight_percent,
     :age_adj_rate
 
@@ -29,5 +29,13 @@ class TopicDemoSerializer < ActiveModel::Serializer
 
   def crude_rate
     object['crude_rate'].present? ? object['crude_rate'] : object['age_adj_rate']
+  end
+
+  def year
+    if object.year_from && object.year_to
+      object.year_from == object.year_to ? object.year_from : [object.year_from.to_s, object.year_to.to_s].join('-')
+    else
+      object.year_from
+    end
   end
 end
