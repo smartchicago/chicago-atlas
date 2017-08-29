ae6b77f0a46e52e3e1ed55eed783f9c4968facb1
mperezdomandtom <marco.perez@domandtom.com>
2017-08-29 14:41:13 -0300

    Removes unnecesary relation

--- app/models/intervention_location.rb
+++ app/models/intervention_location.rb
@@ -1,5 +1,4 @@
 class InterventionLocation < ActiveRecord::Base
   has_many :intervetion_location_datasets
   has_many :service_categories, through: :intervention_location_service_categories
-  belongs_to :geography
 end
