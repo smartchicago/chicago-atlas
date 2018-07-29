d56541bf05c32902026c31de3144edec44f29a4d
gatorjuice <gatorjuice@gmail.com>
2018-07-29 11:33:23 -0500

    added rest of crud methods to controller

--- app/controllers/api/v1/partners_controller.rb
+++ app/controllers/api/v1/partners_controller.rb
@@ -4,6 +4,39 @@ module Api
       def index
         render json: Partner.all
       end
+
+      def create
+        partner = Partner.new(partner_params)
+        if partner.save
+          render json: status: 200
+        else
+          render json: status: 500
+        end
+      end
+
+      def update
+        partner = Partner.find(params[:id])
+        if partner.update(partner_params)
+          render json: status: 200
+        else
+          render json: status: 500
+        end
+      end
+
+      def destroy
+        partner = Partner.find(params[:id])
+        if partner.delete
+          render json: status: 200
+        else
+          render json: status: 500
+        end
+      end
+
+      private
+
+      def partner_params
+        params.require(:partner).permit()
+      end
     end
   end
 end
